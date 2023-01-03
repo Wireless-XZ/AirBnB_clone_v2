96a33d7f24171a397439f02658e41c9511639c31
xz <odufeso1@gmail.com>
2023-01-03 11:07:11 -0800

    updates

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- a script that prepares a MySQL server for the project:
-CREATE IF NOT EXISTS DATABASE hbnb_db;
-CREATE IF NOT EXISTS USER 'hbnb'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT * ON hbnb_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
