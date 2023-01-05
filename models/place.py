4746d1fa9bad4f642b757a54c42dac025c468145
xz <odufeso1@gmail.com>
2023-01-05 07:40:07 -0800

    Updated tasks 7 and 8

--- models/place.py
+++ models/place.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import String, Intger,Float, ForeignKey
+from sqlalchemy import relationship
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = "places"
+
+    if storage_type == 'db':
+        city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
+        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(Integer, nullable=False, default=1)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        amenity_ids = []
+        user = relationship("User", back_populates="places")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
