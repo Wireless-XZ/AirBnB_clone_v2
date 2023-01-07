8dbf9fc33242412bf8ca3a513413d385c65f1bcf
xz <odufeso1@gmail.com>
2023-01-06 20:09:44 -0800

    Made changes to place model

--- models/place.py
+++ models/place.py
@@ -11,10 +11,12 @@ from os import getenv
 storage_type = getenv("HBNB_TYPE_STORAGE")
 metadata = Base.metadata
 place_amenity = Table('place_amenity', metadata,
-                      Column('place_id', String(60), ForeignKey("places.id"),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey("amenities.id"),
-                             primary_key=True, nullable=False)
+                      Column('place_id', String(60),
+                             ForeignKey("places.id"),
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey("amenities.id"),
+                             nullable=False)
                       )
 
 
