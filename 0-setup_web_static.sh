259cb3a0edc91c550c47e5f4e52fef86fceec890
Abdullateef Odufeso <odufeso1@gmail.com>
2023-01-22 03:02:41 +0100

    DEBUGUNG

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,10 +3,11 @@
 # Update Package
 #
 sudo apt update
+sudo apt -y upgrade
 sudo apt -y install nginx
 
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 
 sudo echo "WIRELEXZ..." | sudo tee /data/web_static/releases/test/index.html
 
@@ -15,10 +16,11 @@ ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -hR ubuntu:ubuntu "/data/"
 
-CONFIG="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+CONFIG="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/\
+;\n\t}\n"
 
 FILE="/etc/nginx/sites-available/default"
 
 sudo sed -i "s|server_name _;|$CONFIG|" $FILE
 
-sudo system nginx restart
+sudo service nginx restart
