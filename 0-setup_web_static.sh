606a0f94c1a4441a61a4f228c2d800a50c2b6803
xz <odufeso1@gmail.com>
2023-01-23 15:01:24 +0000

    reverted code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,65 +1,14 @@
 #!/usr/bin/env bash
-#
-# Configures a server for deoloyment
-#
-# Check if Nginx is already installed
-if ! command -v nginx &>/dev/null; then
-    # Install Nginx
-    sudo apt-get update
-    sudo apt-get install nginx -y
-fi
+# set up dummy html
 
-# Create necessary directories if they don't already exist
-if [ ! -d "/data" ]; then
-    sudo mkdir /data
-fi
-
-if [ ! -d "/data/web_static" ]; then
-    sudo mkdir /data/web_static
-fi
-
-if [ ! -d "/data/web_static/releases" ]; then
-    sudo mkdir /data/web_static/releases
-fi
-
-if [ ! -d "/data/web_static/shared" ]; then
-    sudo mkdir /data/web_static/shared
-fi
-
-if [ ! -d "/data/web_static/releases/test" ]; then
-    sudo mkdir /data/web_static/releases/test
-fi
-
-# Create a fake index.html file
-echo "test content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create or update symbolic link
-if [ -L "/data/web_static/current" ]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration
-sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
-EOF
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        server_name _;
-        root /data/web_static/current;
-        index index.html;
-
-        location /hbnb_static/ {
-                alias /data/web_static/current/;
-        }
-}"
-EOF
-
-# Restart Nginx
+server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+file="/etc/nginx/sites-available/default"
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Holberton" > "/data/web_static/releases/test/index.html"
+rm -f "/data/web_static/current"; ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
+sudo sed -i "29i\ $server" "$file"
 sudo service nginx restart
-
-EOF
