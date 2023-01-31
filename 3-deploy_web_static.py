bef9f2b9b3ea5e1cf92c66588583862bcaa70b7b
xz <odufeso1@gmail.com>
2023-01-31 02:29:03 +0000

    made updates

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -62,7 +62,7 @@ def do_deploy(archive_path):
 
 def deploy():
     """  creates and distributes an archive to your web servers """
-    archive_path = do_pack()
+    path = do_pack()
     if archive_path is None:
         return False
 
