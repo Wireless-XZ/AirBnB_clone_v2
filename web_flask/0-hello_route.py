d08eccfabf998b45c6d4341e51af92d21e6fa5ca
xz <odufeso1@gmail.com>
2023-02-15 00:11:04 +0100

    Made updates

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+"""The 0-hello_route module"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """A function that displays Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+"""The 1-hbnb_route module"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """A function that displays Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that displays HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
