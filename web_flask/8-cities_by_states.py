3af508d5efc56d011fc6de6be18a455e8d44859c
xz <odufeso1@gmail.com>
2023-03-10 00:41:50 +0100

    Added new files

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage, State, City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Removes the current SQLAlchemy Session after each request"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all states and their cities"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda s: s.name)
+
+    return render_template('cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
