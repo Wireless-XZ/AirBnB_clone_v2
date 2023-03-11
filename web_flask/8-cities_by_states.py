1d2f52b596cabe5b052a3222bad871fbf47b0f96
xz <odufeso1@gmail.com>
2023-03-11 21:31:53 +0100

    Fixed Bug

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,22 +4,26 @@
 """
 from flask import Flask, render_template
 from models import storage
-from modesls.state import State
-from modesls.city import City
+from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Removes the current SQLAlchemy Session after each request"""
+    """
+        Removes the current SQLAlchemy Session after each request
+    """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with a list of all states and their cities"""
-    states = storage.all(State).values()
+    """
+        Display a HTML page with a list of all states and their cities
+    """
+    states = storage.all('State').values()
     states = sorted(states, key=lambda s: s.name)
 
     return render_template('8-cities_by_states.html', states=states)
